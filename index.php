f473382dbeb10a1f482bddda3a6d4d7a5a9a7304
Cody Evans <codyevansdesign@gmail.com>
2014-01-16 12:49:30 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Grunt Test</title>
+	<link rel="stylesheet" href="css/build/global.css">
+</head>
+<body>
+
+	<h1>Hello World</h1>
+
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+		Repellendus, laboriosam, incidunt, ipsam dolorum illo nam vel quam 
+		numquam fugiat quasi reprehenderit pariatur tempore repudiandae 
+		facilis possimus eveniet autem necessitatibus voluptatum.
+	</p>
+	
+
+	<script src="js/build/production.min.js"></script>
+</body>
+</html>
---
+++ js/global.js
@@ -0,0 +1,8 @@
+
+//alert('hello mother, hello father');
+
+$('h1').text("hello world");
+
+$('p').on('click', function() {
+	$('h1').fadeToggle();
+})
